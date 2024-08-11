431a60ae6c9f598adbe04fe3db136f84d0f53e2e
Antony <atoaquiza17@gmail.com>
2024-08-11 18:35:27 -0500

    adding script to send browser screenshots to telegram

---
+++ sendtelegram.py
@@ -0,0 +1,40 @@
+import os
+from telethon import TelegramClient
+import logging
+import asyncio
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARNING
+)
+
+# I think is more secure to use enviroment variables for remote vm (not sure). In any case the format and commands to set this variables are:
+
+# Linux: export VARIABLE=VALUE
+# Windows: set VARIABLE=VALUE
+
+# NOTE: Environment variables are considered strings
+
+api_id = int(os.getenv('ID_TEL'))
+api_hash = os.getenv('HASH_TEL')
+client = TelegramClient("Main", api_id, api_hash)
+
+async def sendfile():
+    # To get the path the following commands can be used
+    
+    # Linux: pwd
+    # Windows: cd
+    
+    # Alternatively the following variable can be used to just add the file name if the file is in the script folder
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    
+    # NOTE: Linux and windows paths are differents the former uses '/' and the latter '\' and in Python '\' is a escape character so use it twice to denote it as a literal blackslash '\\'
+    # Linux: f'{script_directory}/example.png'
+    # Windows: f'{script_directory}\\example.png'
+    
+    filepath = f'{script_directory}/misc/screenshot.png'
+    
+    await client.send_file('me', filepath)
+    
+with client:
+    client.loop.run_until_complete(send_file())
--- testremote.py
+++ testremote.py
@@ -5,6 +5,8 @@ options = webdriver.EdgeOptions()
 
 options.add_argument('--headless=new')
 
+options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+
 #--------
 
 browser = webdriver.Edge(options=options)
@@ -13,11 +15,15 @@ browser.get('https://google.com')
 
 #--------
 
+browser.save_screenshot('misc/screenshot.png')
+
+#--------
+
 htmlDoc = browser.page_source
 
 soup = BeautifulSoup(htmlDoc, 'html.parser')
 
-with open('SourcePage.txt', 'w') as file:
+with open('misc/sourcepage.txt', 'w') as file:
     file.write(soup.prettify())
 
 browser.quit()
