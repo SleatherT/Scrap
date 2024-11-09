65f105debcaff2073199de8bc8a60ae422a63e49
Antony <atoaquiza17@gmail.com>
2024-11-09 01:19:40 -0500

    Finished telegram api base code, added half of the first command

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import concurrent.futures
 from functools import partial
 
 # Telegram
-from telethon import TelegramClient, events
+import telethon
 
 # Config variables
 import config
@@ -49,36 +49,39 @@ def get_logger():
     return logger
 
 class Telegram_Api():
-    logger = get_logger()
-    
-    _tel_api_id = os.getenv('ID_TEL')
-    _tel_api_hash = os.getenv('HASH_TEL')
-    
     secrets_available = False
-    if not _tel_api_id or not _tel_api_hash:
-        logger.info('Telegram Api data not provided, telegram client will not start and cross-platform commands will not work')
-    else:
-        try:
-            _tel_api_id = int(_tel_api_id)
-        except ValueError:
-            raise ValueError('Telegram Api ID is not a number')
-        
-        secrets_available = True
     
     # Only the messages that start with '!' are valid commands, its possible to change it by only modifying this regex
     valid_command_match = re.compile(r'!\w*').match
     
     # TODO: Since i havent read the documentation about process yet ProcessEvent does nothing right now
-    def __init__(self, *, ThreadEvent=None, ProcessEvent=None):
+    def __init__(self, *, StartedEvent, ThreadEvent=None, ProcessEvent=None):
         
         assert ProcessEvent is None, 'ProcessEvent is not implemented right now'
         
         if not isinstance(ThreadEvent, threading.Event):
             raise TypeError('ThreadEvent argument passed to Telegram_Api is not an instance of threading.Event')
             
+        self.STARTED_EVENT = StartedEvent
         self.THREAD_EVENT = ThreadEvent
         self.PROCESS_EVENT = ProcessEvent
+    
+    @classmethod
+    def get_secrets(cls):
+        logger = get_logger()
+        cls._tel_api_id = os.getenv('ID_TEL')
+        cls._tel_api_hash = os.getenv('HASH_TEL')
         
+        if not cls._tel_api_id or not cls._tel_api_hash:
+            logger.info('Telegram Api data not provided, telegram client will not start and cross-platform commands will not work')
+        else:
+            try:
+                cls._tel_api_id = int(cls._tel_api_id)
+            except ValueError:
+                raise ValueError('Telegram Api ID is not a number')
+            
+            cls.secrets_available = True
+    
     def start(self):
         self.logger = get_logger()
         asyncio.run(self.start_telegram())
@@ -86,16 +89,16 @@ class Telegram_Api():
     async def start_telegram(self):
         # // Login/Starting
         
-        self.t_client = TelegramClient("Main", self._tel_api_id, self._tel_api_hash)
+        self.t_client = telethon.TelegramClient("Main", self._tel_api_id, self._tel_api_hash)
         await self.t_client.start()
         
         # // Adding the handler and pattern to the NewMessage event
         
         # Using the id of the administrator chat from the config file
         telAdminEntity = await self.t_client.get_input_entity(config.t_admin_user)
-        telAdminId = telAdminEntity.user_id
+        self.telAdminId = telAdminEntity.user_id
         
-        self.t_client.add_event_handler(self.process_command, events.NewMessage(chats=telAdminId, pattern=self.check_for_command))
+        self.t_client.add_event_handler(self.process_command, telethon.events.NewMessage(chats=self.telAdminId, pattern=self.check_for_command))
         
         # // Adding a task to periodically check if the thread/process should continue running or should exit
         
@@ -105,7 +108,8 @@ class Telegram_Api():
         # // If ThreadEvent was passed, it signals that has finished starting
         
         if self.THREAD_EVENT:
-            self.THREAD_EVENT.set()
+            self.logger.debug('About to set STARTED_EVENT to signal wait call in Control_Thread')
+            self.STARTED_EVENT.set()
         
         # // Starting to listen updates
         await self.t_client.run_until_disconnected()
@@ -127,11 +131,19 @@ class Telegram_Api():
         fullString = event.pattern_match.string
         command = event.pattern_match.group()[1:]
         self.logger.debug(f'Executing command: {command}')
+        
+        func = self.tel_commands[command]
+        
+        try:
+            result = await func(self, event)
+        except AssertionError as e:
+            self.logger.exception(f'Catched AssertionError')
+            await event.reply(str(e))
     
     async def confirm_execution(self):
         while True:
             await asyncio.sleep(30)
-            self.logger.debug('About to confirm execution')
+            self.logger.debug(f'About to confirm execution. Value: {self.THREAD_EVENT.is_set()}')
             
             if self.THREAD_EVENT.is_set():
                 continue
@@ -140,14 +152,52 @@ class Telegram_Api():
         
         sys.exit()
     
-    async def forward_screenshot(self):
+    async def forward_screenshot(self, event):
         # Is this the correct way to do it?
         filePath = os.path.abspath('misc/screenshots/WAscreenshot.png')
         
         await self.t_client.send_file(self.telAdminEntity, filePath)
     
+    async def reduce_video_resolution_doc(self, event):
+        allowed_mime_types = ['video/x-matroska']
+        
+        message = event.message
+        file = event.file
+        
+        if event.file is None:
+            message = await event.get_reply_message()
+            file = message.file
+            
+            assert message is not None, 'No file in the message and no replied message'
+            
+        
+        assert file is not None, 'Message doesnt contain file'
+        assert file.mime_type in allowed_mime_types, 'mime_type not allowed'
+        
+        
+        ''' If is not possible to do something with the .file then this would be used instead, not sure tho
+        attributesDict = {type(attribute):attribute for attribute in file.attributes}
+        assert telethon.tl.types.DocumentAttributeFilename in attributesDict, 'File has no name, ?'
+        filename = attributesDict[telethon.tl.types.DocumentAttributeFilename].file_name
+        '''
+        
+        assert file.name is not None, 'File has no name, ?'
+        filename = file.name
+        
+        # Copied from the docs
+        def callback(current, total):
+            self.logger.info(f'Downloaded {current} out of {total} {"bytes: {:.2%}".format(current / total)}')
+        
+        path = os.path.abspath(f'misc/downloads/{filename}')
+        
+        returned_path = await message.download_media(path, progress_callback=callback)
+        
+        self.logger(f'Downloaded file; path passed: {path}, path returned: {returned_path}')
+        
+        
+    
     # Saving the telegram codes in dict with their coro
-    tel_commands = {'fwscreenshot': forward_screenshot}
+    tel_commands = {'fwscreenshot': forward_screenshot, 'now': reduce_video_resolution_doc}
     
 
 
@@ -191,17 +241,21 @@ class Control_Thread():
         telegramThreadName = 'telegram'
         threads = list()
         
+        if self.start_telegram:
+            Telegram_Api.get_secrets()
+        
         # NOTE: First it should start the API's and their threads, one at a time
         if self.start_telegram and Telegram_Api.secrets_available:
             telegram_event = self.keepAliveEvents['telegram']
-            telegram_api = Telegram_Api(ThreadEvent=telegram_event)
+            telegram_started_event = threading.Event()
+            telegram_api = Telegram_Api(StartedEvent=telegram_started_event, ThreadEvent=telegram_event)
             telegram_thread = threading.Thread(target=telegram_api.start, name=telegramThreadName)
             
             telegram_thread.start()
             
-            logger.debug('About to call telegram_event.wait')
-            telegram_event.wait()
-            logger.debug('Finished telegram_event.wait')
+            self.logger.debug('About to call telegram_started_event.wait')
+            telegram_started_event.wait()
+            self.logger.debug('Finished telegram_started_event.wait')
             
             threads.append(telegram_thread)
         
@@ -254,7 +308,7 @@ def signal_handler(signum=None, frame=None, *, control_thread_obj):
         logger.info(f'Received signal, signum: {signum}')
     
     if hasattr(control_thread_obj, 'keepAliveEvents'):
-        logger.debug('Confirmed that control_thread has keepAliveEvents attribute')
+        logger.debug(f'Confirmed that control_thread has keepAliveEvents attribute of length: {len(control_thread_obj.keepAliveEvents)}')
         for event in control_thread_obj.keepAliveEvents.values():
             event.clear()
     
@@ -269,32 +323,37 @@ def signal_handler(signum=None, frame=None, *, control_thread_obj):
 
 
 if __name__ == '__main__':
-    logger = get_logger()
-    control_thread_obj = Control_Thread(start_telegram=True)
+    # Storing the main thread references in a local data so it doesnt spread to other threads (added because I found that I may call 
+    # logger instead self.logger and nothing would alert me)
+    mydata = threading.local()
     
-    handler = partial(signal_handler, control_thread_obj=control_thread_obj)
+    mydata.logger = get_logger()
     
-    signal.signal(2, handler)
-    signal.signal(15, handler)
+    mydata.control_thread_obj = Control_Thread(start_telegram=True)
     
-    control_thread = threading.Thread(target=control_thread_obj.start_control_thread, args=[handler], name='Control_Thread')
+    mydata.handler = partial(signal_handler, control_thread_obj=mydata.control_thread_obj)
     
-    control_thread.start()
+    signal.signal(2, mydata.handler)
+    signal.signal(15, mydata.handler)
+    
+    mydata.control_thread = threading.Thread(target=mydata.control_thread_obj.start_control_thread, 
+                                             args=[mydata.handler], name='Control_Thread')
+    mydata.control_thread.start()
     
     # FIX: An awfull, terrible, painfull, abominable, constrained but even worse, bloated code, I didn't found another way of keeping 
     # the main thread active to be able to receive signals without looping on windows, on linux, macOS and other os, locks can receive 
     # POSIX signals so it will be added in the near future
     if sys.platform in ['win32', 'linux', 'darwin']:
-        logger.debug('Starting loop to receive signals')
+        mydata.logger.debug('Starting loop to receive signals')
         while True:
             # INFO: A timeout of 5 seconds, if a signal was sent it will take a max of those seconds to call the handler
-            control_thread.join(timeout=5.0)
+            mydata.control_thread.join(timeout=5.0)
             
-            if not control_thread.is_alive():
+            if not mydata.control_thread.is_alive():
                 break
-            logger.debug('Looped')
-        logger.info('Finishing up main thread, signals will not be processed after this point')
+            mydata.logger.debug('Looped')
+        mydata.logger.info('Finishing up main thread, signals will not be processed after this point')
         
     else:
-        logger.critical('OS doesnt have a way to keep the main thread alive, add a personalized way or use the win32 method')
-        handler()
+        mydata.logger.critical('OS doesnt have a way to keep the main thread alive, add a personalized way or use the win32 method')
+        mydata.handler()
